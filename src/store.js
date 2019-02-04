f231b2f0c377ea7dd62868da8c9da6cf947e4725
Juan Barba <jbarba@rootstack.com>
2019-02-04 14:24:36 -0500

    Implemented local storage so the redux state is kept at every state update

--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers/index";
+import {loadState, saveState} from './localStorage';
+import throttle from 'lodash/throttle';
 
-const store = createStore(rootReducer);
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState);
+
+store.subscribe(throttle(() => {
+  // TODO: do not call the save function every time the state changes
+  saveState({
+    clips: store.getState().clips
+  })
+}, 1000))
+
 export default store;
